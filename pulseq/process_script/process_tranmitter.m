fa9bc38c54baad1ea0f1e774967b2d4a70a6ccde
praveenivp <praveen.ivp@gmail.com>
2024-01-26 13:29:47 +0100

    fix process_transmitter

--- pulseq/process_script/process_tranmitter.m
+++ pulseq/process_script/process_tranmitter.m
@@ -1,9 +1,16 @@
+%% setup variables
+sn='/ptmp/pvalsala/deuterium/EAZW-GUMK/TWIX/';
+
+addpath(genpath('/ptmp/pvalsala/MATLAB'))
+addpath(genpath('/ptmp/pvalsala/Packages/DeuteMetCon'));
+addpath(genpath('/ptmp/pvalsala/Packages/pulseq'));
+
 %% load sequence file
 system = mr.opts('rfRingdownTime', 20e-6, 'rfDeadTime', 100e-6, ...
                  'adcDeadTime', 20e-6);
 
 seq=mr.Sequence(system);              % Create a new sequence object
-seq.read('trans_adjust_2H_1min.seq')
+seq.read(fullfile(sn,'../EXPDATA/pulseq/trans_adjust_2H_1min.seq'))
 
 st.dwell_s=seq.getDefinition('dwell');
 st.fa_array=seq.getDefinition('fa_array');
@@ -11,10 +18,10 @@ st.rf_dur=seq.getDefinition('rf_dur');
 st.averages=seq.getDefinition('averages');
 st.repetitions=seq.getDefinition('repetitions');
 %%
-dirst=dir('meas_MID00358_FID05021_pulseq_trans.dat');
+dirst=dir(fullfile(sn,'allData#S94Tuebingen#F5081#M423#D220124#T103144#pulseq_transmitter.dat'));
 st.filename=dirst(end).name %load last file
 twix=mapVBVD(st.filename);
-twix=twix{end};
+if(iscell(twix)),twix=twix{end}; end
 data=twix.image{''};
   data=reshape(data,size(data,1),size(data,2),st.averages,st.repetitions);
 Spectrum=squeeze(sos(myfft(data,1),[2 3]));
@@ -27,13 +34,17 @@ st.pulseVoltage= (st.fa_array./st.rf_dur).*((st.RefVoltage*1e-3)/pi);
 faxis=linspace(-0.5/st.dwell_s,0.5/st.dwell_s,length(Spectrum));
 
 
-figure
-    subplot(121),plot(faxis,Spectrum),xlabel('Freq [Hz]'),title(st.filename,'Interpreter','none');
+figure,tiledlayout(1,2,"TileSpacing","compact","Padding","compact")
+   nexttile() ,plot(faxis,Spectrum),xlabel('frequency[Hz]'),title(st.filename(27:end),'Interpreter','none');
+     set(gca,'ColorOrder',parula(size(Spectrum,2)));
        xlim([-200 200])
-    subplot(122),plot(st.pulseVoltage,max(Spectrum,[],1)),xlabel('pulse Voltage [V]')
-% subplot(122),plot(rad2deg(st.fa_array),max(Spectrum,[],1))
-
+    nexttile(),plot(st.pulseVoltage,max(Spectrum,[],1),'LineWidth',1.5),xlabel('pulse voltage [V]')
+grid on
+text(1.1*min(xlim),1.1*min(ylim),sprintf('Pulse duration= %.1f ms\n\n  ',st.rf_dur*1e3),'FontSize',12)
 % pulse voltage with maximum amplitude is 90 deg 
 [~,max_idx]=max(max(Spectrum,[],1));
 optimal_referencevoltage=st.pulseVoltage(max_idx)*(st.rf_dur/0.5e-3)
 title(sprintf('current/optimal RefVolatge %.1f V/%.1f V',st.RefVoltage,optimal_referencevoltage))
+ fontsize(gcf,'scale',1.3)
+
+ set(gcf,"Position",[323 325 1516 782])
