0c427b385db29f52f3995dc3012f483ff1f62a68
praveenivp <praveen.ivp@gmail.com>
2024-02-29 22:03:38 +0100

    minor modifications to utils

--- utils/getBandingRegion.m
+++ utils/getBandingRegion.m
@@ -1,27 +1,40 @@
-function sig_amp=getBandingRegion(TR,fmap,metabolties1,FA)
+function sig_amp=getBandingRegion(TR,fmap,metabolites,FA)
 %sig_amp=getBandingRegion(TR,fmap,metabolties1,FA)
 % function to get banding region depending on fieldmap,TR and metabolite
-
-
 % sig_amp=getBandingRegion(10e-3,fmap_rad_s,metabolties1,40);
+%
+%INPUTS:
+% TR : Repetition time in s
+% fmap : 1H fieldmap rad_s
+% metabolites : struct
+% FA : flip angle in radians
+
+
+
+
+if( isobject(TR))
+    mcobj=TR;
+    TR=mcobj.DMIPara.TR;
+    fmap=mcobj.FieldMap;
+    metabolites=mcobj.metabolites;
+    FA=mcobj.DMIPara.FlipAngle;
+
+end
 if(FA>2)
     warning('converting FA into radians')
     FA=deg2rad(FA);
 end
+if(exist('metabolties','var'))
+    cs_ppm=[0 1.0256 ]*1e-6; %D20 Glu Glx Lac/fat
+    freq_shift_WGX=[3.9 -58.7  -152 -215];
+    T1=[432.88 69.65 147.6 190.64]*1e-3;%s
+    T2=[287 65.88 124.5 180]*1e-3;%s
 
-
-cs_ppm=[0 1.0256 ]*1e-6; %D20 Glu Glx Lac/fat
-freq_shift_WGX=[3.9 -58.7  -152 -215];
-T1=[432.88 69.65 147.6 190.64]*1e-3;%s
-T2=[287 65.88 124.5 180]*1e-3;%s
-
-
-
-sp_name={'D20','Glucose','Glutamate','Lactate'};
-for i=1:4 
-    metabolites(5-i)=struct('T1_s',T1(i),'T2_s',T2(i),'freq_shift_Hz',freq_shift_WGX(i),'name',sp_name{i});
+    sp_name={'D20','Glucose','Glutamate','Lactate'};
+    for i=1:4
+        metabolites(5-i)=struct('T1_s',T1(i),'T2_s',T2(i),'freq_shift_Hz',freq_shift_WGX(i),'name',sp_name{i});
+    end
 end
-
 
 
 % fmap in 2H hz
